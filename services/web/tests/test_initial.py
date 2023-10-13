4cd8f4a0c42a60f5a75c3f26b6952d86797541d2
tomi <tomiwest@hotmail.com>
2023-10-13 16:05:07 +0300

    push

--- services/web/tests/test_initial.py
+++ services/web/tests/test_initial.py
@@ -11,6 +11,11 @@ def test_endpoint_returns_all_users(test_client):
     print(os.getenv("TEST_DATABASE_URL"))
     assert response.json == [{"email": "michael@mherman.org", "id": 1}]
 
+def test_dummy(test_client):
+    
+    
+    assert 1 == 2
+
 
 
 
